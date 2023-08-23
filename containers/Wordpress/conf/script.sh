a3a578e489cde93c40fe6dcbc9ae6bc5d4349cf4
achrafainhajar <achrafainhajar@gmail.com>
2023-08-23 09:50:47 +0100

    test

---
+++ containers/Wordpress/conf/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+chown -R www-data:www-data /var/www/html/wordpress
+
+mkdir -p /run/php
+touch /run/php/php7.3-fpm.sock
+chown -R www-data:www-data /run/php/
+chmod 660 /run/php/php7.3-fpm.sock
+cd /var/www/html/wordpress
+wp core download --allow-root
+wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
+wp core install --url=${DOMAIN_NAME} --title=achraf --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
+wp user create john john@example.com --role=author --user_pass=password --allow-root
+exec $@
